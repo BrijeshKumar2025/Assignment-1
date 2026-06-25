fa118ff267bdd81e7805311e56237410cb6008a9
BrijeshKumar2025 <bk4868945@gmail.com>
2026-06-25 21:45:06 +0530

    push

---
+++ flag.js
@@ -0,0 +1,185 @@
+let mainFrame = document.getElementById("main-frame");
+mainFrame.classList = "border-2 p-2 flex";
+let flagbox = document.createElement("div");
+
+
+
+function getindiaFlag() {
+  let indiaFlag = document.createElement("div");
+  indiaFlag.classList = "border-2 w-31 m-2";
+
+let orangeStrap = document.createElement("div");
+orangeStrap.classList = "h-10 w-30 bg-orange-500";
+
+let whiteStrap = document.createElement("div");
+whiteStrap.classList = "h-10 w-30 justify-items-center";
+
+let greenStrap = document.createElement("div");
+greenStrap.classList = "h-10 w-30 bg-green-500";
+
+let blueDics = document.createElement("div");
+blueDics.classList = "bg-blue-700 w-10 h-10 rounded-full";
+whiteStrap.appendChild(blueDics);
+
+indiaFlag.appendChild(orangeStrap);
+indiaFlag.appendChild(whiteStrap);
+indiaFlag.appendChild(greenStrap);
+return indiaFlag;
+}
+function getjapanFlag() {
+let japanFlag = document.createElement("div");
+japanFlag.classList = "border-2 w-31 h-31 justify-items-center content-center m-2";
+
+let redDics = document.createElement("div");
+redDics.classList = "bg-red-700 w-10 h-10 rounded-full";
+japanFlag.appendChild(redDics);
+return japanFlag;
+}
+
+function getbanglaFlag() {
+let banglaFlag = document.createElement("div");
+banglaFlag.classList ="border-2 w-31 h-31 justify-items-center content-center m-2 bg-green-800";
+
+let red2Dics = document.createElement("div");
+red2Dics.classList = "bg-red-700 w-10 h-10 rounded-full";
+banglaFlag.appendChild(red2Dics);
+return banglaFlag;
+}
+function getrusFlag() {
+let rusFlag = document.createElement("div");
+rusFlag.classList = "border-2 w-31 m-2";
+
+let white2Strap = document.createElement("div");
+white2Strap.classList = "h-10 w-30 justify-items-center";
+
+let blueStrap = document.createElement("div");
+blueStrap.classList = "h-10 w-30 bg-blue-600";
+
+let redStrap = document.createElement("div");
+redStrap.classList = "h-10 w-30 bg-red-500";
+
+rusFlag.appendChild(white2Strap);
+rusFlag.appendChild(blueStrap);
+rusFlag.appendChild(redStrap);
+return rusFlag;
+}
+function getromaniaFlag() {
+let romaniaFlag = document.createElement("div");
+romaniaFlag.classList = "flex border-2 w-31 m-2 ";
+
+let blue2Strap = document.createElement("div");
+blue2Strap.classList = "h-30 w-10 bg-blue-600";
+
+let yellowStrap = document.createElement("div");
+yellowStrap.classList = "h-30 w-10 bg-yellow-400";  
+
+let red3Strap = document.createElement("div");
+red3Strap.classList = "h-30 w-10 bg-red-500";
+romaniaFlag.appendChild(blue2Strap);
+romaniaFlag.appendChild(yellowStrap);
+romaniaFlag.appendChild(red3Strap);
+return romaniaFlag;
+}
+function getperuFlag() {
+let peruFlag = document.createElement("div");
+peruFlag.classList = "flex border-2 w-31 m-2 ";
+
+let laalStrap = document.createElement("div");
+laalStrap.classList = "h-30 w-10 bg-red-700";
+
+let safedStrap = document.createElement("div");
+safedStrap.classList = "h-30 w-10 bg-white";  
+
+let laal2Strap = document.createElement("div");
+laal2Strap.classList = "h-30 w-10 bg-red-700";
+peruFlag.appendChild(laalStrap);
+peruFlag.appendChild(safedStrap);
+peruFlag.appendChild(laal2Strap);
+return peruFlag;
+}
+function polandFlag() {
+let polandFlag = document.createElement("div");
+polandFlag.classList = " border-2 w-31 m-2 ";
+
+let white3Strap = document.createElement("div");
+white3Strap.classList = "h-15 w-30 bg-white";
+
+let red2Strap = document.createElement("div");
+red2Strap.classList = "h-15 w-30 bg-red-500";
+polandFlag.appendChild(white3Strap);
+polandFlag.appendChild(red2Strap);
+return polandFlag;
+}
+function getnigeriaFlag() {
+let nigeriaFlag = document.createElement("div");
+nigeriaFlag.classList = "flex border-2 w-30 m-2 ";
+
+let nigeriagreenStrap = document.createElement("div");
+nigeriagreenStrap.classList = "h-30 w-7 bg-emerald-500";
+
+let white4Strap = document.createElement("div");
+white4Strap.classList = "h-30 w-16  bg-white";  
+
+let nigeriagreen2Strap = document.createElement("div");
+nigeriagreen2Strap.classList = "h-30 w-7 bg-emerald-500";
+nigeriaFlag.appendChild(nigeriagreenStrap);
+nigeriaFlag.appendChild(white4Strap);
+nigeriaFlag.appendChild(nigeriagreen2Strap);
+return nigeriaFlag;
+}
+
+
+
+let countryname="india";
+let input = document.createElement("input");
+input.classList = " h-14 border-2 p-2 m-2" ;
+input.placeholder = "Enter country name";
+
+let searchbutton = document.createElement("button");
+
+searchbutton.innerText = "Search Country";
+searchbutton.classList = " h-14 border-2 p-2 m-2 bg-blue-500 text-white";
+searchbutton.addEventListener("click", () => {
+  let country = input.value;
+  input.value = "";
+  countryname = country;
+  //flagbox.appendChild("");
+  flagbox.innerHTML = "";
+
+if (countryname == "india") {
+  flagbox.appendChild(getindiaFlag());
+} 
+else if (countryname == "japan") {
+  flagbox.appendChild(getjapanFlag());
+}
+else if (countryname == "bangladesh") {
+  flagbox.appendChild(getbanglaFlag());
+}
+else if (countryname == "russia") {
+  flagbox.appendChild(getrusFlag());
+}
+else if (countryname == "romania") {
+  flagbox.appendChild(getromaniaFlag());
+}
+else if (countryname == "poland") {
+  flagbox.appendChild(polandFlag());
+}
+else if (countryname == "nigeria") {
+  flagbox.appendChild(getnigeriaFlag());
+}
+else if (countryname == "peru") {
+  flagbox.appendChild(getperuFlag());
+}
+else {
+  let error = document.createElement("div");
+  error.classList = "text-red-500";
+  error.innerText = "Country not found";
+  flagbox.appendChild(error);
+}
+console.log(countryname);
+});
+
+
+mainFrame.appendChild(input); 
+mainFrame.appendChild(searchbutton);
+mainFrame.appendChild(flagbox);
--- lib.js
+++ lib.js
@@ -1,7 +1,7 @@
 // Printing library books details
 const library = [
     { bookId: 101, title: "Learn English with Ram", author: "ram Chander", category: "English language", copies: 5 },
-    { bookId: 102, title: "Computer", author: "Monu", category: "Database", copies: 2 },
+    { bookId: 102, title: "Computer Science", author: "Monu", category: "Database", copies: 2 },
     { bookId: 103, title: "Computer Networks", author: "Sonu", category: "Networking", copies: 1 },
     { bookId: 104, title: "Data Structures", author: "Shivam", category: "Programming", copies: 4 },
     { bookId: 105, title: "Web Development", author: "Prince", category: "Programming", copies: 10 },
@@ -19,21 +19,17 @@ const library = [
 
 for(let i=0; i<library.length;i++)
 {
-    console.log(library[i].bookId);
-    console.log(library[i].title);
-    console.log(library[i].author);
-    console.log(library[i].category);
-    console.log(library[i].copies);
+    console.log("bookId:",library[i].bookId);
+    console.log("title:",library[i].title);
+    console.log("author:",library[i].author);
+    console.log("category:",library[i].category);
+    console.log("copies:",library[i].copies);
     console.log("=================================================================================================")
 }
 //Task 2 counting of all library books
 console.log("Total books:---");
-let totalBooks=0;
-for(let i=0; i<library.length;i++)
-{
-    totalBooks+=library[i].copies;
-}
-console.log(totalBooks);
+let totalbooks = () =>library.length;
+console.log("Total Books:",totalbooks());
 console.log("=====================================================================================================")
 //Task 3  Book with low availability
 console.log("Books with low availability:---");
@@ -41,37 +37,81 @@ for(let i=0; i<library.length;i++)
     {
     if(library[i].copies<3)
     {
-        console.log(library[i].title,library[i].copies);
+        console.log(library[i].title,"-",library[i].copies);
     }
 }
 console.log("=====================================================================================================")
 //Task 4
+console.log("Task 4 Search with book id:---");
+let bookid=109;
+let searchBookById = (id) => {
+    for(let i=0; i<library.length;i++)
+    {
+        if(library[i].bookId === id)
+        {
+            console.log("Book Found");
+            console.log("Title:",library[i].title);
+            console.log("Author:",library[i].author);
+            break;
+        }
+        else if(i===library.length-1){
+               console.log("Book Not Found");
+        }
+        
+    }
+}
+searchBookById(bookid);
 console.log("=====================================================================================================")
 //Task 5 Category book wise count
+console.log("task 5:---");
+let a=0,b=0,c=0,d=0,e=0;
 for(let i=0; i<library.length;i++)
     {
     if(library[i].category === 'Programming')
     {
-        console.log(library[i].category,library[i].copies);
+        a+=library[i].copies;
     }
     else if(library[i].category === 'Database')
     {
-        console.log(library[i].category,library[i].copies);
+        b+=library[i].copies;
     }
     else if(library[i].category === 'Networking')
     {
-        console.log(library[i].category,library[i].copies);
+        c+=library[i].copies;
     }
-    else {
-        console.log(library[i].category,library[i].copies);
+    else if(library[i].category === 'Computer Science'){
+        d+=library[i].copies;
+    }
+    else if(library[i].category === 'Mathematics'){
+        e+=library[i].copies;
     }
 }
+console.log("Programming:", a);
+console.log("Database:", b);
+console.log("Networking:", c);
+console.log("Computer Science:", d);
+console.log("Mathematics:", e); 
 console.log("=====================================================================================================")
 //Task 6
-let maxCopies=0;
-
+console.log("Task 6:---");
+let maxCopies=() => {
+let max=library[0].copies;
+let i;
+    for(i=0; i<totalbooks();i++)
+    {
+        if(library[i].copies>max)
+        {
+            max=library[i].copies;
+        }
+    }
+    return max;
+}
+for(let i=0; i<library.length;i++){
+    if(library[i].copies===maxCopies())
+console.log(library[i].title,"-",maxCopies(),"copies");}
 console.log("=====================================================================================================")
 //Task 7
+console.log("Task 7:---");
 for(let i=0; i<library.length;i++)
 {
     let status;
@@ -87,12 +127,12 @@ for(let i=0; i<library.length;i++)
     {
         status="Critical";
     }
-    console.log(library[i].title, status);
+    console.log(library[i].title,"-", status);
 }
 
 console.log("=====================================================================================================")
 //Task 8
-console.log("Count book in each status:---");
+console.log("Task 8: Count book in each status:---");
 let Available=0;
 let Limited=0;
 let Critical=0; 
@@ -116,7 +156,7 @@ console.log("Limited:", Limited);
 console.log("Critical:", Critical);
 console.log("=====================================================================================================")
 //Task 9
-console.log("Display Programming books:---");
+console.log("Task 9:Display Programming books:---");
 for(let i=0; i<library.length;i++)
     {
     if(library[i].category === 'Programming')
@@ -127,7 +167,8 @@ for(let i=0; i<library.length;i++)
 //Task 10
 console.log("Display Library summary report:---")
 console.log("---------------------LIBRARY REPORT-----------------------")
-console.log("Total Books:", totalBooks);
+console.log("Total Books:", totalbooks());
 console.log("Available Books:", Available);
 console.log("Limited Books:", Limited);
 console.log("Critical Books:", Critical);
+console.log("Highest Copies Available:",maxCopies());
